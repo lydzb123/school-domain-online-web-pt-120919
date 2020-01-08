df17ff8eefc97734f70528ee92b7f40214f94805
Lydia B <lydiabaik@gmail.com>
2020-01-08 16:56:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+attr_accessor :name, :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student(student, grade)
+  roster[grade] ||= []
+  roster[grade] << student
+end
+
+def grade(level)
+  if roster[grade] == level
+    roster[grade]
+  end
+
+end
+
+
+end
